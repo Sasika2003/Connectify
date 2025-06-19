8f14481a982dc08101f1ec690ab8d62388d0cb4e
SASIKA <114210043+Sasika2003@users.noreply.github.com>
2025-06-19 21:22:15 +0530

    DB deployment completed

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # config.py
 
 class Config:
-   SECRET_KEY = 'your_secret_key'
+    SECRET_KEY = 'your_secret_key'
 
-    # Remote MySQL Configuration (InfinityFree)
-    MYSQL_HOST = os.getenv('DB_HOST', 'sql210.infinityfree.com')
-    MYSQL_USER = os.getenv('DB_USER', 'if0_39273264')
-    MYSQL_PASSWORD = os.getenv('DB_PASSWORD', 'your_password_here')
-    MYSQL_DB = os.getenv('DB_NAME', 'if0_39273264_connectify')
-    MYSQL_PORT = int(os.getenv('DB_PORT', 3306))
+    MYSQL_HOST = 'sql210.infinityfree.com'
+    MYSQL_USER = 'if0_39273264'
+    MYSQL_PASSWORD = 'Sasika2003'
+    MYSQL_DB = 'if0_39273264_connectify'
+    MYSQL_PORT = 3306
+
 UPLOAD_FOLDER = '/path/to/upload/folder'
