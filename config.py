9ba1f33b3e49c3650e0b40246e3db970fdf8d9f8
SASIKA <114210043+Sasika2003@users.noreply.github.com>
2025-06-19 21:20:24 +0530

    updated db deployment

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 # config.py
 
 class Config:
-    SECRET_KEY = 'your_secret_key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'sasika1234'
-    MYSQL_DB = 'social_media_db'
+   SECRET_KEY = 'your_secret_key'
+
+    # Remote MySQL Configuration (InfinityFree)
+    MYSQL_HOST = os.getenv('DB_HOST', 'sql210.infinityfree.com')
+    MYSQL_USER = os.getenv('DB_USER', 'if0_39273264')
+    MYSQL_PASSWORD = os.getenv('DB_PASSWORD', 'your_password_here')
+    MYSQL_DB = os.getenv('DB_NAME', 'if0_39273264_connectify')
+    MYSQL_PORT = int(os.getenv('DB_PORT', 3306))
 UPLOAD_FOLDER = '/path/to/upload/folder'
